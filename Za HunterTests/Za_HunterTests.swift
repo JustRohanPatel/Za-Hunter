8124026ad1c5807b5aa699bd35441eba909a0f12
rohan patel <rpatel5@bsd220.org>
2025-02-05 09:40:39 -0600

    Initial Commit

---
+++ Za HunterTests/Za_HunterTests.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterTests.swift
+//  Za HunterTests
+//
+//  Created by Rohan Patel on 2/5/25.
+//
+
+import Testing
+@testable import Za_Hunter
+
+struct Za_HunterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

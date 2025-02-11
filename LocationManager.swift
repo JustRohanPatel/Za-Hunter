652f31cf2ff8f1ded128c978911814c76a095ccd
rohan patel <rpatel5@bsd220.org>
2025-02-11 10:00:31 -0600

    Added pizza icon and link to store website

---
+++ LocationManager.swift
@@ -0,0 +1,21 @@
+//
+//  LocationManager.swift
+//  Za Hunter
+//
+//  Created by Rohan Patel on 2/11/25.
+//
+
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+    var geocoder = CLGeocoder()
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
--- Za Hunter/ContentView.swift
+++ Za Hunter/ContentView.swift
@@ -1,24 +1,61 @@
 //
 //  ContentView.swift
-//  Za Hunter
+//  Map Hackwitch
 //
 //  Created by Rohan Patel on 2/5/25.
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    @State private var startPosition = MapCameraPosition.userLocation(fallback: .automatic)
+    @State private var places = [Place]()
+    @State private var mapRegion = MKCoordinateRegion()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Map(position: $startPosition) {
+                UserAnnotation()
+                ForEach(places) { place in
+                    Annotation(place.mapItem.name!, coordinate: place.mapItem.placemark.coordinate) {
+                        if let url = place.mapItem.url {
+                            Link(destination: url, label: {
+                                Image("pizza")
+                            })
+                            
+                        }
+                    }
+                }
+            }
         }
-        .padding()
+        .onMapCameraChange {content in
+            mapRegion = content.region
+            performSearch(item: "Pizza")
+        }
     }
+    func performSearch(item: String) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = item
+        searchRequest.region = mapRegion
+        let search = MKLocalSearch(request: searchRequest)
+        search.start() { response, error in
+            if let response = response {
+                places.removeAll()
+                for mapItem in response.mapItems {
+                    places.append(Place(mapItem: mapItem))
+                }
+            }
+        }
+    }
+}
+    
+struct Place: Identifiable {
+    let id = UUID()
+    let mapItem: MKMapItem
 }
 
 #Preview {
     ContentView()
 }
+
